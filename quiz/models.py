320336d5b64bbd5f3409a82d3567eba840857745
Apurv <24apurv.deshpande@gmail.com>
2020-02-08 17:27:01 +0530

    Models created

---
+++ quiz/models.py
@@ -0,0 +1,54 @@
+from django.db import models
+from django.core.validators import RegexValidator
+
+
+class MyUser(models.Model):
+	phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$',
+ 								message="Phone number must be entered in the format: '+999999999'")
+	name_1 = models.CharField(max_length=100, verbose_name='Participant 1')
+	email_1 = models.EmailField(unique=True, verbose_name='Participant 1 Email')
+	phone_number_1 = models.CharField(validators=[phone_regex], max_length=17, blank=False, verbose_name='Participant 1 Phone Number')
+	name_2 = models.CharField(max_length=100, null=True, blank=True, verbose_name='Participant 2')
+	email_2 = models.EmailField(unique=True, null=True, blank=True, verbose_name='Participant 2 Email')
+	phone_number_2 = models.CharField(validators=[phone_regex], max_length=17, blank=True, verbose_name='Participant 2 Phone Number')
+	team_name = models.CharField(max_length=100, null=True, blank=True)
+
+	def __str__(self):
+		if team_name:
+			return team_name
+		else:
+			return name1
+
+
+class Result(models.Model):
+	user = models.OneToOneField(MyUser, on_delete=models.CASCADE)
+	score = models.IntegerField(null=False)
+
+	def __str__(self):
+		return user
+
+
+class Question(models.Model):
+	statement = models.CharField(max_length=500, verbose_name='Question statement')
+	is_active = models.BooleanField(default=True, verbose_name='Active', help_text='Should question be included in quiz?')
+
+	def __str__(self):
+		return statement
+
+
+ANSWER_CHOICES = [
+	('A','A'),
+	('B','B'),
+	('C','C'),
+	('D','D'),
+	]	
+class Answer(models.Model):
+	question = models.ForeignKey(Question, on_delete=models.CASCADE)
+	option_a = models.CharField(max_length=500, blank=False, null=False, verbose_name='Option A')
+	option_b = models.CharField(max_length=500, blank=False, null=False, verbose_name='Option B')
+	option_c = models.CharField(max_length=500, blank=False, null=False, verbose_name='Option C')
+	option_d = models.CharField(max_length=500, blank=False, null=False, verbose_name='Option D')
+	correct_answer = models.CharField(max_length=2, blank=False, null=False, choices=ANSWER_CHOICES)
+
+	def __str__(self):
+		return question
